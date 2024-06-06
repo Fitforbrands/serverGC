5b13756151488f0a8c508a68d625bee6c91b804b
gabrielcapuia <gabriel@fitforbrands.com>
2024-06-06 20:45:48 -0300

    cambio url fetch a servidor propio 2

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ import express from "express";
 import bodyParse from "body-parser";
 import { readFile } from "fs/promises"; // para importar un archivo JSON
 const app = express();
+
 app.use(bodyParse.urlencoded({ extended: true }));
 app.use(bodyParse.json());
 
 const PORT = process.env.PORT || 3977;
 const fletes = await readFile("./fletes.json", "utf-8"); // leyendo el archivo JSON
+const listado = JSON.parse(fletes);
 
 app.get("/saludo", (req, res) => {
   res.send("Bienvenido a nuestro sitio");
 });
 
 app.get("/fletes", (req, res) => {
-  res.send(fletes);
+  res.send(listado);
 });
 
 app.listen(PORT, () => {
