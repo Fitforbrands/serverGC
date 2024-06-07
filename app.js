c13d53e888c2fe0262129da6373253c6eac89c52
gabrielcapuia <gabriel@fitforbrands.com>
2024-06-07 05:15:25 -0300

    Cors limitado a url de netlify 2

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const corsoptions = {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      calback(new Error("Not allowed by CORS"));
+      callback(new Error("Not allowed by CORS"));
     }
   },
 };
