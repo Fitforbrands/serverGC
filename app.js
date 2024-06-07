b700cba3440bbcb7e846d4bbce737441eea13fd7
gabrielcapuia <gabriel@fitforbrands.com>
2024-06-07 05:00:46 -0300

    Cors limitado a url de netlify

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ import { readFile } from "fs/promises"; // para importar un archivo JSON
 import cors from "cors";
 
 const app = express();
+const whitelist = ["https://fletesjs.netlify.app/"];
+const corsoptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      calback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
+app.use(cors(corsoptions));
 app.use(cors());
 app.use(bodyParse.urlencoded({ extended: true }));
 app.use(bodyParse.json());
